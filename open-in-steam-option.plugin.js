01143d81887567b573b714900283bdbee5984164
Ahrisuwu <161222302+Ahrisuwu@users.noreply.github.com>
2024-02-25 19:06:05 +0100

    Add files via upload

---
+++ open-in-steam-option.plugin.js
@@ -0,0 +1,57 @@
+/**
+ * @name OpenInSteamOption
+ * @author Ahrisuwu
+ * @description Adds "Open Link In Steam" option to message context menu.
+ * @version 1.0.1
+ * @authorId 1108751432850604102
+ */
+
+module.exports = class {
+      unpatchFunctions = [];
+      steamDomains = [ "steamcommunity.com", "help.steampowered.com", "store.steampowered.com" ];
+
+      start()
+      {
+            this.unpatchFunctions.push(BdApi.ContextMenu.patch("message", (contextMenu, message) => {
+
+                  console.log(message);
+                  let url = message.target.href ?? message.target.innerText;
+                  if (!url || !this.isSteamURL(url)) return;
+
+                  let menuItem = BdApi.ContextMenu.buildItem({ 
+                        id: "open-link-in-steam", 
+                        label: "Open Link In Steam", 
+                        action: () => this.openLinkInSteam(url) 
+                  });
+                  contextMenu.props.children.push(menuItem);
+            }));
+            
+            this.unpatchFunctions.push(BdApi.ContextMenu.patch("image-context", (contextMenu, message) => {
+
+                  let url = message.href;
+                  if (!url || !this.isSteamURL(url)) return;
+
+                  let menuItem = BdApi.ContextMenu.buildItem({ 
+                        id: "open-link-in-steam",
+                        label: "Open Link In Steam", 
+                        action: () => this.openLinkInSteam(url) 
+                  });
+                  contextMenu.props.children.props.children.push(menuItem);
+            }));
+      }
+
+      stop()
+      {
+            this.unpatchFunctions.forEach(unpatchFn => unpatchFn());;
+      }
+
+      isSteamURL(url)
+      {
+            return this.steamDomains.some(domain => url.startsWith("https://" + domain));
+      }
+
+      openLinkInSteam(url)
+      {
+            window.open("steam://openurl/" + url);
+      }
+}
